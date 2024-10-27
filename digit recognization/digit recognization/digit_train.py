fdca4ac9d27c76b97723ace0614a7518e84450eb
manojna2004 <96356864+manojna2004@users.noreply.github.com>
2024-10-27 20:08:47 +0530

    Add files via upload

---
+++ digit recognization/digit recognization/digit_train.py
@@ -0,0 +1,43 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Flatten
+from keras.layers import Conv2D,MaxPooling2D
+from keras import backend as K
+
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+x_train=x_train.reshape(x_train.shape[0],28,28,1)
+x_test=x_test.reshape(x_test.shape[0],28,28,1)
+input_shape=(28,28,1)
+
+y_train=keras.utils.to_categorical(y_train,10)
+y_test=keras.utils.to_categorical(y_test,10)
+
+x_train=x_train.astype("float32")
+x_test=x_test.astype("float32")
+
+x_train/=255
+x_test/=255
+
+batch_size=128
+num_classes=10
+epochs=20
+
+model=Sequential()
+model.add(Conv2D(64,kernel_size=(5,5),activation='relu',input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(64,kernel_size=(5,5),activation='relu'))
+model.add(Flatten())
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.3))
+
+model.add(Dense(num_classes,activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+hist = model.fit(x_train, y_train, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
+
+score=model.evaluate(x_test,y_test,verbose=0)
+print("loss",score[0])
+print("accuracy",score[1])
+model.save('mnist.h5')
